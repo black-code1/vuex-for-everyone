d99ba3cf8435d68a001b0b23fca8b0ccd93ade95
black-code1 <legrandmahamat1@gmail.com>
2023-01-09 11:57:12 +0100

    Vuex Mutations

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -9,17 +9,18 @@
 
 <script>
 import shop from "../api/shop";
+import store from "../store/index";
 export default {
   name: "ProductList",
-  data(){
-    return {
-      products: []
+  computed: {
+    products() {
+      return store.state.products
     }
   },
 
   created() {
     shop.getProducts(products => {
-      this.products = products
+      store.commit('setProducts', products)
     })
   }
 }
